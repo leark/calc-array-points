68f8c63ce42368a58ae06fd43bc5828ddc5d0be6
Seung Lee <shl7@uw.edu>
2016-10-10 17:42:36 -0700

    implemented add, subtract, multiply, divide, tuples

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -8,5 +8,68 @@
 
 import Foundation
 
-print("Hello, World!")
+func add(left: Int, right: Int) -> Int {
+    return left + right
+}
+
+func subtract(left: Int, right: Int) -> Int {
+    return left - right
+}
+
+func multiply(left: Int, right: Int) -> Int {
+    return left * right
+}
+
+func divide(left: Int, right: Int) -> Int {
+    return left / right
+}
+
+func mathOperation(left: Int, right: Int, operation: (Int, Int) -> Int) -> Int {
+    return operation(left, right)
+}
+
+func add(array: [Int]) -> Int {
+    var result = 0
+    for num in array {
+        result += num
+    }
+    return result
+}
+
+func multiply(array: [Int]) -> Int {
+    var result = 1
+    for num in array {
+        result *= num
+    }
+    return result
+}
+
+func count(array: [Int]) -> Int {
+    return array.count
+}
+
+func average(array: [Int]) -> Int {
+    return add(array: array) / array.count
+}
+
+func reduce(array: [Int], operation: ([Int]) -> Int) -> Int {
+    return operation(array)
+}
+
+func add(p1: (x: Int, y: Int), p2: (x: Int, y: Int)) -> (x: Int, y: Int) {
+    return (x: p1.x + p2.x, y: p1.y + p2.y)
+}
+
+func subtract(p1: (x: Int, y: Int), p2: (x: Int, y: Int)) -> (x: Int, y: Int) {
+    return (x: p1.x - p2.x, y: p1.y - p2.y)
+}
+
+func add(p1: [String: Int], p2: [String: Int]) -> [String: Int] {
+    return ["x": p1["x"]! + p2["x"]!, "y": p1["y"]! + p2["y"]!]
+}
+
+func subtract(p1: [String: Int], p2: [String: Int]) -> [String: Int]{
+    return ["x": p1["x"]! - p2["x"]!, "y": p1["y"]! - p2["y"]!]
+}
+
 
